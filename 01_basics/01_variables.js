a59068a9f229ff20e270f05ecb9954ab499cbfc2
Rishabh Singh Yadav <rishabhrsy123@gmail.com>
2025-06-17 00:46:53 +0530

    started with variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountid=12345
+let accountemail="rishabh@gmail.com"
+var accountpass="123456789"
+accountcity="Kota"
+
+// accountid=19703   not allowed
+
+accountemail="Risha@gmail.com"
+accountpass="00000"
+accountcity='Delhi'
+
+console.log(accountid)
+
+console.table([accountid,accountemail,accountpass,accountcity
