126e4ccf8b103bff3a8b16b731f13b67f8e9f94e
Rishabh Singh Yadav <rishabhrsy123@gmail.com>
2025-06-18 23:36:00 +0530

    learnt variable and constant

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,14 +1,14 @@
-const accountid=12345
+ const accountid=12345                    // defines a constant 
 let accountemail="rishabh@gmail.com"
 var accountpass="123456789"
 accountcity="Kota"
 
-// accountid=19703   not allowed
+// accountid=19703                         // (not allowed because it's a constant)
 
 accountemail="Risha@gmail.com"
-accountpass="00000"
+accountpass="00000"                       //These variable values can be changed 
 accountcity='Delhi'
 
 console.log(accountid)
 
-console.table([accountid,accountemail,accountpass,accountcity
+console.table([accountid,accountemail,accountpass,accountcity])      //returns a table with the passed attributes
