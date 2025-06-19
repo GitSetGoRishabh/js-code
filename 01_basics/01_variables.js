59967e2b939b4d03c7adac69cfa56389abc39079
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-06-19 23:31:38 +0530

    completed variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@
 let accountemail="rishabh@gmail.com"
 var accountpass="123456789"
 accountcity="Kota"
+let accountstate;
 
 // accountid=19703                         // (not allowed because it's a constant)
 
@@ -12,4 +13,10 @@ accountcity='Delhi'
 console.log(accountid)                    // will return the constant accountid
 
 
-console.table([accountid,accountemail,accountpass,accountcity])      //returns a table with the passed attributes
+/*
+try not to use var method 
+beacause of issue in block scope and functional scope 
+let is the best method
+*/
+
+console.table([accountid,accountemail,accountpass,accountcity,accountstate])      //returns a table with the passed attributes
