904aa020799117a477e137cf26b561c0f4e9aa71
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-06-22 12:23:51 +0530

    data types

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,3 +2,26 @@
 
 // alert(3 + 3)       // it won't run here bcz it runs on browser (go to inspect-->console to try)
 
+let myname = "Rishabh"  //string
+let number= 18        //number
+let isboy = true      //boolean
+let age
+let failedsubs=null
+/*
+Data Types :
+
+number --> 2 to the power 53
+bigint --> larger than number 
+string --> ""
+boolean--> true/false
+null   --> standalone value
+undefined-->
+symbol --> for unique
+object
+
+*/
+
+console.log(typeof myname)
+console.log(typeof number)
+console.log(typeof age)
+console.log(typeof failedsubs)
