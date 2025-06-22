16bce2ec7ca18c2950b33e10ac384f0e83a11823
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-06-22 23:52:10 +0530

    revised datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+/*
+for same data type works normally 
+>,<,==,!=
+*/
+
+// console.log("2">1)
+// console.log("02">1)
+
+// here it is converting them because they are different
+
+// console.log(null > 0)
+// console.log(null== 0)
+// console.log(null>=0)
+
+/*
+equality == checks it as it is so nul is not equal to 0
+but comparison(<,>,<=,>=) converts null into a number i.e. 0
+*/
+
+// console.log(undefined > 0)
+// console.log(undefined== 0)
+// console.log(undefined>=0)
+
+// false in every case not like null
+// avoid these comparisons
---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,38 @@
+/*
+primitive --> call by value
+7 types:
+string
+number
+boolean
+null
+undefined
+symbol
+bigint
+
+Non primitive --> call by reference
+Array
+objects
+functions
+*/
+
+const id=Symbol('123')
+const otherid=Symbol('123')
+
+console.log(id===otherid)
+const bignumber= 4156415348451235n
+
+
+
+const heroes=["shaktiman","nagraj"]
+let obj={
+    name:"Rishabh",
+    age:100
+}
+
+const myfunction=function(){
+    console.log("I am a good boy!")
+}
+
+myfunction()
+console.log(heroes)
+console.log(obj)
