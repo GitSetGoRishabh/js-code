926f6b4bb7213585bead9a4c17d591a49c376d2f
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-07-06 20:44:20 +0530

    object-3 and JSON

---
+++ 02_baiscs/05_objects.js
@@ -0,0 +1,25 @@
+const course = {
+    name: "Ameeri",
+    price: "9",
+    Instructor:"rishabh"
+}
+
+// course.Instructor
+
+// destructuring
+const{Instructor: ins} = course
+console.log(ins)
+
+//JSON 
+
+// {
+//     "name":"rishabh",
+//     "courseName":"Gareebi",
+//     "price":"9999",
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
