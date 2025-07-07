19a70863299794e2adc343d1280a56461d9f4409
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-07-07 22:14:22 +0530

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,6 +1,7 @@
-function addTwoNumbers(num1,num2){
-    console.log(num1+num2)
-}
+// function addTwoNumbers(num1,num2){
+//     console.log(num1+num2)
+// }
+
 
 // addTwoNumbers(3,4)
 // addTwoNumbers()  ---> returns Nan as nothing is given
@@ -12,24 +13,55 @@ function addTwoNumbers(num1,num2){
 
 // console.log("result:", newNum)  //--> gives undefined
 
-function addNumbers(n1,n2){
-    // let result = n1+n2
-    // return result
-    return n1+n2
-}
+// function addNumbers(n1,n2){
+//     // let result = n1+n2
+//     // return result
+//     return n1+n2
+// }
 
-const result = addNumbers(4,9)
+// const result = addNumbers(4,9)
 // console.log("result:", result)
 
-function loggedInMessage(username){
-    if(username === undefined){
-        console.log("Please enter a username")
-        return
-    }
-    return `${username} just logged in`
+// function loggedInMessage(username){
+//     if(username === undefined){
+//         console.log("Please enter a username")
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+// console.log(loggedInMessage("rishabh"))
+// console.log(loggedInMessage(""))
+// console.log(loggedInMessage())
+
+// function calculateCartPrice(...num1){ //--> rest operator (...) 
+//     // takes all arguements and wraps it into an array to the parameter
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200,300,400))
+ 
+// const user = {
+//     username : "rishabh",
+//     price : 99
+// }
+
+// function handleObject(anyobject){
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+// }
+
+// handleObject(user);
+
+// handleObject({
+//     username:"Risha",
+//     price:1000
+// })
+
+const myNewArray = [200,400,600,100]
+
+function returnSecondValue(getArray){
+    return getArray[1];
 }
 
-console.log(loggedInMessage("rishabh"))
-console.log(loggedInMessage(""))
-console.log(loggedInMessage())
- 
+console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([100,200,300]))
