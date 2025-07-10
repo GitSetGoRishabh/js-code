79ffee805cb25a238ea54967e8102780ef309b0b
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-07-11 01:13:16 +0530

    control flow

---
+++ 04_control-flow/one.js
@@ -0,0 +1,26 @@
+/*
+--> if
+
+if(condition){
+    (scope)
+}
+
+ if true then always goes , if false then never goes
+
+ */
+
+const isUserLoggedIn = true
+
+if(isUserLoggedIn){
+    console.log("logged in")
+}
+
+
+
+/*
+
+<,>,<=,>=,==,!=
+
+=== (checks type as well),!==(checks type as well as value(complement of ===))
+
+*/
