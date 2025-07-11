0babe32d6213d9813ca3d64bd4cd0ef92189ded9
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-07-11 18:52:00 +0530

    switch

---
+++ 04_control-flow/02_switch.js
@@ -0,0 +1,44 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 8
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    case 5:
+        console.log("May")
+        break;
+    case 6:
+        console.log("June")
+        break;
+    case 7:
+        console.log("July")
+        break;
+    case 8:
+        console.log("August")
+        break;
+
+    default:
+        console.log("default case matched"); 
+        break;
+}
+
+// if break is not there then all cases after the matched case are run except default
+// if there is string then same as well
