28dc8a4f781c4a22eb6397865f9ede21da1fffdc
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-07-12 23:41:23 +0530

    while and do while

---
+++ 05_iterations/02_while-do-while.js
@@ -0,0 +1,33 @@
+//while
+// while (condition) {
+    
+// }
+
+// let index=0
+
+// while (index<=10) {
+//     console.log("hehe");
+//     index++;
+// }
+
+// let myArray = ['rishabh','singh','yadav']
+// let arr = 0
+// while (arr<myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++;
+// }
+
+// do while
+
+// do {
+    
+// } while (condition);
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score<=10);
+
+// it atleast runs once even if condition not true
